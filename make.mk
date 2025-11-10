ca45165e7217acf5ab25f8bb8ef812c598139a32
Kacper Jelonek <kapijelonek@gmail.com>
2025-11-10 20:11:43 +0100

    make create build directory

--- make.mk
+++ make.mk
@@ -3,34 +3,47 @@ CC       = $(PREFIX)-gcc
 OC       = $(PREFIX)-objcopy
 SZ       = $(PREFIX)-size
 
+PROJECT_DIR := $(CURDIR)
+
 MAKEFILE_DIR := $(dir $(lastword $(MAKEFILE_LIST)))
 
-LINKER   = $(MAKEFILE_DIR)/Link.ld
+LINKER = $(MAKEFILE_DIR)/Link.ld
 
-FLAGS = -Os -march=rv32ec -mabi=ilp32e -nostdlib -ffreestanding -Wall -ffunction-sections -Wl,-Map,$(TARGET).map,-T$(LINKER)
+BUILD_DIR = $(PROJECT_DIR)/build
+
+$(shell mkdir -p $(BUILD_DIR))
+
+ELF = $(BUILD_DIR)/$(TARGET).elf
+BIN = $(BUILD_DIR)/$(TARGET).bin
+MAP = $(BUILD_DIR)/$(TARGET).map
+
+FLAGS = -Os -march=rv32ec_zicsr_zifencei -mabi=ilp32e -nostdlib -ffreestanding -Wall -ffunction-sections \
+        -Wl,-Map,$(MAP),-T$(LINKER)
 
 SRC += $(MAKEFILE_DIR)/startup.S
 
 all: clean build flash
 
-build: $(TARGET).bin
+build: $(BIN)
 	@echo
-	@$(SZ) --format=berkeley $(TARGET).elf
+	@$(SZ) --format=berkeley $(ELF)
 	@echo
-	@$(eval FLASH_USED=$(shell $(SZ) -A $(TARGET).elf | awk '/\.text|\.data/{sum+=$$2} END{print sum}'))
-	@$(eval RAM_USED=$(shell $(SZ) -A $(TARGET).elf | awk '/\.data|\.bss/{sum+=$$2} END{print sum}'))
+	@$(eval FLASH_USED=$(shell $(SZ) -A $(ELF) | awk '/\.text|\.data/{sum+=$$2} END{print sum}'))
+	@$(eval RAM_USED=$(shell $(SZ) -A $(ELF) | awk '/\.data|\.bss/{sum+=$$2} END{print sum}'))
 	@echo "Flash: $(FLASH_USED) bytes (≈$$(echo $$(($(FLASH_USED)*100/16384)))%)"
 	@echo "RAM:   $(RAM_USED) bytes (≈$$(echo $$(($(RAM_USED)*100/2048)))%)"
 	@echo
 
-$(TARGET).elf: $(SRC)
+$(ELF): $(SRC)
+	@mkdir -p $(BUILD_DIR)
 	$(CC) $(FLAGS) -o $@ $^
 
-$(TARGET).bin: $(TARGET).elf
+$(BIN): $(ELF)
 	$(OC) -O binary $< $@
 
-flash: $(TARGET).bin
-	wlink flash $(TARGET).bin
+flash: $(BIN)
+	wlink flash $(BIN)
 
 clean:
-	$(RM) *.elf *.bin *.map
+	$(RM) -r $(BUILD_DIR)
+
